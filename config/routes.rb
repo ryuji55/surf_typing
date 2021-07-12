e0ad5b2288008cf73d0c3631d570cd7b9e472af5
ryuji55 <rt.october.2@icloud.com>
2021-07-12 09:02:42 +0900

    Add:linkとルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   resources :typings, only: %i[index new]
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy', to: 'static_pages#privacy'
 end
