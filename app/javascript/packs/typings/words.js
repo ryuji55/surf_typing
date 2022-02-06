ad1ecc4da68c655d480951c9c435f428ce324590
ryuji55 <rt.october.2@icloud.com>
2022-02-07 04:22:24 +0900

    Fix:問題文とbase64をモジュール化

---
+++ app/javascript/packs/typings/words.js
@@ -0,0 +1,378 @@
+export const words = [
+      {
+        jp: '今日1日を楽しく生きてみる',
+        en: 'kyouitinitiwotanosikuikitemiru'
+      },
+      {
+        jp: '成長に夢中になる',
+        en: 'seityounimutyuuninaru'
+      },
+      {
+        jp: '好きな人と一緒にいなさい',
+        en: 'sukinahitotoissyoniinasai'
+      },
+      {
+        jp: '先を明るく見よう',
+        en: 'sakiwoakarukumiyou'
+      },
+      {
+        jp: '好きなことをやっていきなさい',
+        en: 'sukinakotowoyatteikinasai'
+      },
+      {
+        jp: '大切な人を大切に',
+        en: 'taisetunahitowotaisetuni'
+      },
+      {
+        jp: '疑うより信じてみよう',
+        en: ['utagauyorisinnjitemiyou','utagauyorisinnzitemiyou']
+      },
+      {
+        jp: 'もう過ぎたことは忘れよう',
+        en: 'mousugitakotohawasureyou'
+      },
+      {
+        jp: '幸せは当たり前の中にある',
+        en: 'siawasehaatarimaenonakaniaru'
+      },
+      {
+        jp: 'いい事は嫌な事の後にやってくる',
+        en: 'iikotohaiyanakotonoatoniyattekuru'
+      }, //10
+      {
+        jp: '感謝の気持ちを忘れない',
+        en: 'kannsyanokimotiwowasurenai'
+      },
+      {
+        jp: 'いつも心に青空を',
+        en: 'itumokokoroniaozorawo'
+      },
+      {
+        jp: '今を生きる、今を大切にする',
+        en: 'imawoikiru,imawotaisetunisuru'
+      },
+      {
+        jp: '人は人、自分と向き合う事を優先する',
+        en: ['hitohahito,jibunntomukiaukotowoyuusennsuru','hitohahito,zibunntomukiaukotowoyuusennsuru']
+      },
+      {
+        jp: '自分の気持ちを大切に',
+        en: ['jibunnnokimotiwotaisetuni','zibunnnokimotiwotaisetuni']
+      },
+      {
+        jp: 'あなたの代わりはどこにもいない',
+        en: 'anatanokawarihadokonimoinai'
+      },
+      {
+        jp: '心に嘘はつけない',
+        en: 'kokoroniusohatukenai'
+      },
+      {
+        jp: 'いいじゃん！失敗しても死ぬわけでもないし',
+        en: ['iijann!sippaisitemosinuwakedemonaisi','iizyann!sippaisitemosinuwakedemonaisi']
+      },
+      {
+        jp: '暗い部屋でひとり悩むことはやめよう',
+        en: 'kuraiheyadehitorinayamukotohayameyou'
+      },
+      {
+        jp: '信じれば晴れる',
+        en: ['sinnjirebahareru','sinnzirebahareru']
+      },//20
+      {
+        jp: 'もっと笑ってみてもいいんじゃない？',
+        en: ['mottowarattemitemoiinnjanai?','mottowarattemitemoiinnzyanai?']
+      },
+      {
+        jp: '晴れわたる日も雨の日も',
+        en: 'harewataruhimoamenohimo'
+      },
+      {
+        jp: 'もっと肩の力を抜くと全てうまくいく',
+        en: 'mottokatanotikarawonukutosubeteumakuiku'
+      },
+      {
+        jp: 'ありのままのあなたが素敵です',
+        en: 'arinomamanoanatagasutekidesu'
+      },
+      {
+        jp: '前を向いていないと波には乗れない',
+        en: 'maewomuiteinaitonaminihanorenai'
+      },
+      {
+        jp: '悲しくても大丈夫！悲しみが人を深くする',
+        en: ['kanasikutemodaijoubu!kanasimigahitowohukakusuru','kanasikutemodaizyoubu!kanasimigahitowohukakusuru']
+      },
+      {
+        jp: '雨が降るから虹もでる',
+        en: ['amegahurukaranijimoderu','amegahurukaranizimoderu']
+      },
+      {
+        jp: '自分を愛するから人を愛せる',
+        en: ['jibunnwoaisurukarahitowoaiseru','zibunnwoaisurukarahitowoaiseru']
+      },
+      {
+        jp: '迷ったら自分を好きでいられる方を選ぶ',
+        en: ['mayottarajibunnwosukideirareruhouwoerabu','mayottarazibunnwosukideirareruhouwoerabu']
+      },
+      {
+        jp: '結果をすぐに求めない後からついてくる物だから',
+        en: 'kekkawosugunimotomenaiatokaratuitekurumonodakara'
+      },//30
+      {
+        jp: 'うまくできなくてもいい！心を込める',
+        en: 'umakudekinakutemoii!kokorowokomeru'
+      },
+      {
+        jp: '今死ぬとしたらこれまでの人生をどう思う？',
+        en: ['imasinutositarakoremadenojinnseiwodouomou?','imasinutositarakoremadenozinnseiwodouomou?']
+      },
+      {
+        jp: 'あなたと好きな人が幸せでいれますように',
+        en: 'anatatosukinahitogasiawasedeiremasuyouni'
+      },
+      {
+        jp: 'いつも今いる場所から前を向く',
+        en: 'itumoimairubasyokaramaewomuku'
+      },
+      {
+        jp: '今自分にできる事から始める',
+        en: ['imajibunnnidekirukotokarahajimeru','imazibunnnidekirukotokarahazimeru']
+      },
+      {
+        jp: 'もっと挑戦しよう',
+        en: 'mottotyousennsiyou'
+      },
+      {
+        jp: 'どうしても許せないときは許せない自分を許そう',
+        en: ['dousitemoyurusenaitokihayurusenaijibunnwoyurusou','dousitemoyurusenaitokihayurusenaizibunnwoyurusou']
+      },
+      {
+        jp: '過去ではなく未来と向き合う',
+        en: 'kakodehanakumiraitomukiau'
+      },
+      {
+        jp: '必要以上の悩みは紙に書き出して捨ててしまおう',
+        en: ['hituyouijounonayamihakaminikakidasitesutetesimaou','hituyouizyounonayamihakaminikakidasitesutetesimaou']
+      },
+      {
+        jp: '好きな人の幸せを願える私でいよう',
+        en: 'sukinahitonosiawasewonegaeruwatasideiyou'
+      },//40
+      {
+        jp: 'よく寝てよく学びよく食べてよく遊ぶ',
+        en: 'yokuneteyokumanabiyokutabeteyokuasobu'
+      },
+      {
+        jp: '己の弱さと向き合う',
+        en: 'onorenoyowasatomukiau'
+      },
+      {
+        jp: '強い人はみな優しい',
+        en: 'tuyoihitohaminayasasii'
+      },
+      {
+        jp: 'あなたの事が大切で大好きです',
+        en: 'anatanokotogataisetudedaisukidesu'
+      },
+      {
+        jp: 'あなたは間違っていない',
+        en: 'anatahamatigatteinai'
+      },
+      {
+        jp: '悔しさをバネにして飛躍する',
+        en: 'kuyasisawobanenisitehiyakusuru'
+      },
+      {
+        jp: '失うから新しく得る事ができる',
+        en: 'usinaukaraatarasikuerukotogadekiru'
+      },
+      {
+        jp: 'たまにこけちゃうくらいに真っ直ぐなあなたが好き',
+        en: 'tamanikoketyaukurainimassugunaanatagasuki'
+      },
+      {
+        jp: '何があっても心身の健康を大切に',
+        en: 'nanigaattemosinnsinnnokennkouwotaisetuni'
+      },
+      {
+        jp: '不安な気持ちを味方にする',
+        en: 'huannnakimotiwomikatanisuru'
+      },//50
+      {
+        jp: '好きだから別れる事もある',
+        en: 'sukidakarawakarerukotomoaru'
+      },
+      {
+        jp: '笑ってよ！せめて今だけでも',
+        en: 'waratteyo!semeteimadakedemo'
+      },
+      {
+        jp: '離れてもあなたの幸せを願う',
+        en: 'hanaretemoanatanosiawasewonegau'
+      },
+      {
+        jp: '僕だけをずっと見ていてくれてありがとう',
+        en: 'bokudakewozuttomiteitekuretearigatou'
+      },
+      {
+        jp: '君が笑ったときが好きだったから',
+        en: 'kimigawarattatokigasukidattakara'
+      },
+      {
+        jp: 'あたたかい優しさを心の中に持つ',
+        en: 'atatakaiyasasisawokokorononakanimotu'
+      },
+      {
+        jp: 'これからの人生で今日より若い日は1日もない',
+        en: ['korekaranojinnseidekyouyoriwakaihihaitinitimonai','korekaranozinnseidekyouyoriwakaihihaitinitimonai']
+      },
+      {
+        jp: '一度止まって振り返ると正しいという字になる',
+        en: ['itidotomattehurikaerutotadasiitoiujininaru','itidotomattehurikaerutotadasiitoiuzininaru']
+      },
+      {
+        jp: 'カッコつけないカッコ良さがある',
+        en: 'kakkotukenaikakkoyosagaaru'
+      },
+      {
+        jp: '心変わりを受け入れる度量を持つ',
+        en: 'kokorogawariwoukeirerudoryouwomotu'
+      },//60
+      {
+        jp: 'ビビっていたら波には乗れない',
+        en: 'bibitteitaranaminihanorenai'
+      },
+      {
+        jp: '夢や目標を生きがいにする',
+        en: 'yumeyamokuhyouwoikigainisuru'
+      },
+      {
+        jp: 'シンプルに生きてみる',
+        en: 'sinnpuruniikitemiru'
+      },
+      {
+        jp: '不快感から目を背けず向き合う',
+        en: 'hukaikannkaramewosomukezumukiau'
+      },
+      {
+        jp: '朝は起きるにかぎる',
+        en: 'asahaokirunikagiru'
+      },
+      {
+        jp: '海のように心を広く大きくしていこう',
+        en: 'uminoyounikokorowohirokuookikusiteikou'
+      },
+      {
+        jp: '面白くしたいならノリと勢いを大切にする',
+        en: 'omosirokusitainaranoritoikioiwotaisetunisuru'
+      },
+      {
+        jp: '本音で話し合える仲間は宝物',
+        en: 'honnnedehanasiaerunakamahatakaramono'
+      },
+      {
+        jp: '今笑えてるのは一緒に泣いてくれた君がいたから',
+        en: 'imawaraeterunohaissyoninaitekuretakimigaitakara'
+      },
+      {
+        jp: '辛い経験をよかったと言えるような日々を過ごす',
+        en: 'turaikeikennwoyokattatoieruyounahibiwosugosu'
+      },//70
+      {
+        jp: 'それを素敵と思えるあなたが素敵',
+        en: 'sorewosutekitoomoeruanatagasuteki'
+      },
+      {
+        jp: '痛みから逃げずに受け入れると成長できる',
+        en: 'itamikaranigezuniukeirerutoseityoudekiru'
+      },
+      {
+        jp: '否定される事に慣れないと成長は望めない',
+        en: 'hiteisarerukotoninarenaitoseityouhanozomenai'
+      },
+      {
+        jp: '日々努力、日々成長',
+        en: 'hibidoryoku,hibiseityou'
+      },
+      {
+        jp: '自信は表に出すのではなく内に秘めておく',
+        en: ['jisinnhaomotenidasunodehanakuutinihimeteoku','zisinnhaomotenidasunodehanakuutinihimeteoku']
+      },
+      {
+        jp: '信じるのでも疑うのでもなく確かめてみる',
+        en: ['sinnjirunodemoutagaunodemonakutasikametemiru','sinnzirunodemoutagaunodemonakutasikametemiru']
+      },
+      {
+        jp: 'あなたにとって私もそうでありたい',
+        en: 'anatanitottewatasimosoudearitai'
+      },
+      {
+        jp: '今は信じて続けろ',
+        en: ['imahasinnjitetudukero','imahasinnzitetudukero']
+      },
+      {
+        jp: '己の人生に夢中になれば小さいことは気にならなくなる',
+        en: ['onorenojinnseinimutyuuninarebatiisaikotohakininaranakunaru','onorenozinnseinimutyuuninarebatiisaikotohakininaranakunaru']
+      },
+      {
+        jp: '仲間が困っていたら助ける',
+        en: 'nakamagakomatteitaratasukeru'
+      },//80
+      {
+        jp: '完璧なんてよくない成るようになる',
+        en: 'kannpekinannteyokunainaruyouninaru'
+      },
+      {
+        jp: '挑戦するのに遅すぎることはない',
+        en: 'tyousennsurunoniososugirukotohanai'
+      },
+      {
+        jp: 'どんな出来事からも学ぶ姿勢を忘れない',
+        en: 'donnnadekigotokaramomanabusiseiwowasurenai'
+      },
+      {
+        jp: '同じ波は２度と来ない同じ日も２度と来ない',
+        en: ['onajinamihanidotokonaionajihimonidotokonai','onazinamihanidotokonaionazihimonidotokonai']
+      },
+      {
+        jp: '何かを得るという事は何かを捨てるという事',
+        en: 'nanikawoerutoiukotohananikawosuterutoiukoto' 
+      },
+      {
+        jp: '痛みから学べる事が驚くほどたくさんある',
+        en: 'itamikaramanaberukotogaodorokuhodotakusannaru' 
+      },
+      {
+        jp: '自信のあるフリをしていると自信がついてくる',
+        en: ['jisinnnoaruhuriwositeirutojisinngatuitekuru','zisinnnoaruhuriwositeirutozisinngatuitekuru']
+      },
+      {
+        jp: '己の人生の主人公として生きるんだよ',
+        en: ['onorenojinnseinosyujinnkoutositeikirunndayo','onorenozinnseinosyuzinnkoutositeikirunndayo']
+      },
+      {
+        jp: '己を変えるチャンスはいくらでもある',
+        en: 'onorewokaerutyansuhaikurademoaru'
+      },
+      {
+        jp: '積極的に攻めていこう',
+        en: 'sekkyokutekinisemeteikou'
+      },//90
+      {
+        jp: '時間がないお金がないと言う人はあってもやらない',
+        en: ['jikannganaiokaneganaitoiuhitohaattemoyaranai','zikannganaiokaneganaitoiuhitohaattemoyaranai'] 
+      },
+      {
+        jp: '本気で取り組むときは前向きな言葉を使おう',
+        en: 'honnkidetorikumutokihamaemukinakotobawotukaou' 
+      },
+      {
+        jp: '物事にいいも悪いもないあるのは捉え方だけ',
+        en: 'monogotoniiimowaruimonaiarunohatoraekatadake'
+      },
+      {
+        jp: '努力をすれば成功は必ずしないが成長は必ずする',
+        en: 'doryokuwosurebaseikouhakanarazusinaigaseityouhakanarazusuru'
+      }
+    ];
